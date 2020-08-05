06fea819c2b132be6138586d24d6a50c9cc0a9b8
wangchifu <wangchifu@yahoo.com.tw>
2020-08-05 11:08:36 +0800

    remove au

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,6 @@
     <div class="container">
         <p class="m-0 text-center text-white">
             Copyright &copy;  2019 彰化縣教育處學管科 <br>
-            <small>程式設計：ET Wang</small>
         </p>
     </div>
 </footer>
